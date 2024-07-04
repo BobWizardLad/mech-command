80fba7f27b557d2986c5e60440bb9699a698532c
BobWizardLad <bobwizardlad@Gmail.com>
2024-07-04 13:48:10 -0500

    :art: Comments!
    Code Cleanup

--- Mech-command/Flare.gd
+++ Mech-command/Flare.gd
@@ -5,14 +5,11 @@ extends CharacterBody2D
 @export var rot_speed: float = 5
 @export var proj_speed: float = 400
 
-var height: float = 1
-var flare_target: Vector2 = position
-var origin_pos: Vector2 = position
-var flare_travel: bool = false
-var travel_arc_timing
-
-func _ready():
-	pass
+var height: float = 1 # The projectile 'height': scalar for object scale
+var flare_target: Vector2 = position # Flare landing position
+var origin_pos: Vector2 = position # Relative launch position
+var flare_travel: bool = false # Is the projectile moving?
+var travel_arc_timing # Calculated travel time between launch and landing
 
 func _physics_process(delta) -> void:
 	# TEST input to fire flare
@@ -24,32 +21,40 @@ func _physics_process(delta) -> void:
 		var dir = position.direction_to(flare_target)
 		velocity = dir.normalized() * proj_speed
 	
+	# Flare motion in the x/y plane; seperate from the 'height' traversal
 	if origin_pos < flare_target and not position >= flare_target:
 		move_and_slide()
 	elif origin_pos > flare_target and not position <= flare_target:
 		move_and_slide()
+	# Constant calculation of the scale relative to the proj 'height'
+	scale = Vector2.ONE * height
 	
 	# Flare rotates as it travels
 	var tween = get_tree().create_tween()
 	tween.tween_property(Sprite, 'rotation', Sprite.rotation + rot_speed, 0.3).set_trans(Tween.TRANS_CIRC)
 	
-	scale = Vector2.ONE * height
-	
+	# Activate flare travel time
 	if flare_travel:
-		flare_ascent()
+		flare_ascent(travel_arc_timing)
 		flare_travel = false
 
+# Set the flare arc varaibles relative to parameters
+# target is the target landing spot of the flare
 func set_flare_travel(target: Vector2) -> Node:
 	flare_travel = true
 	flare_target = target
 	travel_arc_timing = flare_target.distance_to(self.position)/proj_speed
 	return self
 
-func flare_descent() -> void:
+# Calls the tween for the second half of the flare arc
+# Defaults to the node's built in arc; can be overwritten
+func flare_descent(arc: float = travel_arc_timing) -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, 'height', 1, (travel_arc_timing)/2).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, 'height', 1, (arc)/2).set_trans(Tween.TRANS_SINE)
 
-func flare_ascent() -> void:
+# Calls the tween for the first half of the flare arc
+# Defaults to the node's built in arc; can be overwritten
+func flare_ascent(arc: float = travel_arc_timing) -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, 'height', 3, (travel_arc_timing)/2).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, 'height', 3, (arc)/2).set_trans(Tween.TRANS_SINE)
 	tween.tween_callback(flare_descent)
--- Mech-command/Player.gd
+++ Mech-command/Player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-
 const SPEED: float = 300.0
 
 func _physics_process(delta) -> void:
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction in both axis and handle direction
 	var horizontal = Input.get_axis("player_left", "player_right")
 	var vertical = Input.get_axis("player_up", "player_down")
 	var direction = Vector2(horizontal, vertical).normalized()
 	
+	# Execute tween of velocity in direction by SPEED
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, 'velocity', direction * SPEED, 0.13).set_trans(Tween.TRANS_SINE)
 
