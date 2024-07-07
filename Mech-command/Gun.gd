f1aadc5578d83ac9043af817cfff6eaddd1f1fee
BobWizardLad <bobwizardlad@Gmail.com>
2024-07-07 11:16:11 -0500

    :sparkles: Reload Timer
    Reload timer will delay firing for a set delay before allowing to shoot again!

--- Mech-command/Gun.gd
+++ Mech-command/Gun.gd
@@ -2,26 +2,35 @@ extends Node2D
 class_name Gun
 
 @onready var COOLDOWN: Timer = $Cooldown
+@onready var RELOAD: Timer = $Reload
 
 @export var projectile: Resource
-@export var firing: bool = false
 @export var firing_cooldown: float
 @export var target: Vector2 = Vector2.ZERO
+@export var mag_size: int
+@export var reload_time: float
 
+var firing: bool = false
+var mag_count: int
 var next_bullet: Chainbullet
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	COOLDOWN.wait_time = firing_cooldown
+	RELOAD.wait_time = reload_time
+	mag_count = mag_size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if firing and COOLDOWN.is_stopped():
+	if firing and COOLDOWN.is_stopped() and mag_count > 0:
 		next_bullet = projectile.instantiate()
 		next_bullet.bullet_target(target)
 		next_bullet.position = global_position
 		get_parent().get_parent().add_child(next_bullet)
+		mag_count -= 1
 		COOLDOWN.start()
+	elif firing and mag_count <= 0:
+		RELOAD.start()
 
 func set_firing(is_firing: bool) -> Gun:
 	firing = is_firing
@@ -30,3 +39,6 @@ func set_firing(is_firing: bool) -> Gun:
 func set_target(target_pos: Vector2) -> Gun:
 	target = target_pos
 	return self
+
+func load_mag():
+	mag_count = mag_size
