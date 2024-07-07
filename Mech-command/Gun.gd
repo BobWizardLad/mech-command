ab1668c2af31cb376a8ac019c78afbc382296f96
BobWizardLad <bobwizardlad@Gmail.com>
2024-07-07 10:50:56 -0500

    :sparkle: Cooldown Timer
    Minigun has a cooldown timer between firing.

--- Mech-command/Gun.gd
+++ Mech-command/Gun.gd
@@ -1,24 +1,27 @@
 extends Node2D
 class_name Gun
 
+@onready var COOLDOWN: Timer = $Cooldown
+
 @export var projectile: Resource
 @export var firing: bool = false
+@export var firing_cooldown: float
 @export var target: Vector2 = Vector2.ZERO
 
 var next_bullet: Chainbullet
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	COOLDOWN.wait_time = firing_cooldown
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if firing:
+	if firing and COOLDOWN.is_stopped():
 		next_bullet = projectile.instantiate()
 		next_bullet.bullet_target(target)
 		next_bullet.position = global_position
 		get_parent().get_parent().add_child(next_bullet)
-		
+		COOLDOWN.start()
 
 func set_firing(is_firing: bool) -> Gun:
 	firing = is_firing
