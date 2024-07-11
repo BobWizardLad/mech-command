b8dca3947093c9d605a211410125ba71798b6bb7
BobWizardLad <bobwizardlad@Gmail.com>
2024-07-11 11:49:24 -0500

    :sparkles: Bot Navigation
    Bots will now use navigation regions and agents to traverse the map to targets. The current target is the player at all times, this will change to a dynamic system later on. Bots will try to avoid each other and will come within 150 pixels of the player to hover around them.

--- Mech-command/Bot.gd
+++ Mech-command/Bot.gd
@@ -1,11 +1,43 @@
 extends CharacterBody2D
-class_name Bot
 
-@export var SPEED: float = 200.0
+var movement_speed: float = 200.0
+var movement_target_position: Vector2
 
-@onready var HEALTH: Health = $Health
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
-var nav_target: Vector2
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(get_parent().get_parent().find_child("Player").position)
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target - position.direction_to(movement_target) * 150
 
 func _physics_process(delta):
-	pass #move_and_slide()
+	set_movement_target(get_parent().get_parent().find_child("Player").position)
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	var new_velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	
+	if navigation_agent.avoidance_enabled:
+		navigation_agent.set_velocity(new_velocity)
+	else:
+		_on_navigation_agent_2d_velocity_computed(new_velocity)
+	
+	move_and_slide()
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	velocity = safe_velocity
