5010375dfe52764f2e8d5e9272d70145ed5452c1
BobWizardLad <bobwizardlad@Gmail.com>
2024-07-11 10:28:11 -0500

    :lipstick: Heath Hide
    Hide health when it is full, only show health bars above damaged units.

--- Mech-command/health.gd
+++ Mech-command/health.gd
@@ -10,10 +10,13 @@ var current_health: float
 func _ready():
 	current_health = HEALTH_MAX
 	HEALTH_BAR.max_value = HEALTH_MAX
+	HEALTH_BAR.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	HEALTH_BAR.value = current_health
+	if HEALTH_BAR.value != HEALTH_MAX:
+		HEALTH_BAR.show()
 
 	if current_health <= 0:
 		get_parent().queue_free()
